619742043090c9919b8b1f564ff03bd02ef75bc5
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2024-10-10 19:59:47 +0900

    feat: API Exception 구현

---
+++ backend/src/main/java/cotato/backend/common/exception/ApiException.java
@@ -0,0 +1,34 @@
+package cotato.backend.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+
+@Getter
+public class ApiException extends RuntimeException {
+
+	private final HttpStatus httpStatus;
+	private final String code;
+
+	public ApiException(
+		HttpStatus httpStatus,
+		String message,
+		String code
+	) {
+		super(message);
+		this.httpStatus = httpStatus;
+		this.code = code;
+	}
+
+	public static ApiException from(ErrorCode errorCode) {
+		return new ApiException(errorCode.getHttpStatus(), errorCode.getMessage(), errorCode.getCode());
+	}
+
+	public static ApiException of(
+		HttpStatus httpStatus,
+		String message,
+		String code
+	) {
+		return new ApiException(httpStatus, message, code);
+	}
+}
