67b3341455e938b2bdde93f65864d6899f07643c
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2024-10-10 19:59:11 +0900

    feat: ErrorCode 구현

---
+++ backend/src/main/java/cotato/backend/common/exception/ErrorCode.java
@@ -0,0 +1,26 @@
+package cotato.backend.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+	//400
+	BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다.", "COMMON-001"),
+	INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "요청 파라미터가 잘 못 되었습니다.", "COMMON-002"),
+
+	//404
+	MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 회원을 찾을 수 없습니다.", "MEMBER-004"),
+
+	//500
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부에서 에러가 발생하였습니다.", "COMMON-002"),
+	;
+
+	private final HttpStatus httpStatus;
+	private final String message;
+	private final String code;
+}
