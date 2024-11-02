53895aa9b09bb68e5aa588c1102daa37aa14fc5b
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2024-11-02 17:29:17 +0900

    feat: Post 엔티티 구현

--- backend/src/main/java/cotato/backend/domains/post/Post.java
+++ backend/src/main/java/cotato/backend/domains/post/Post.java
@@ -1,6 +1,12 @@
 package cotato.backend.domains.post;
 
+import static jakarta.persistence.GenerationType.*;
+
+import cotato.backend.domains.post.dto.request.SavePostRequest;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,4 +16,39 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post {
 
+	@Id
+	@Column(name = "post_id")
+	@GeneratedValue(strategy = IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String title;
+
+	@Column(nullable = false)
+	private String content;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private Long views;
+
+	public Post(
+		String title,
+		String content,
+		String name
+	) {
+		this.title = title;
+		this.content = content;
+		this.name = name;
+		this.views = 0L;
+	}
+
+	public static Post of(SavePostRequest savePostRequest) {
+		return new Post(
+			savePostRequest.getTitle(),
+			savePostRequest.getContent(),
+			savePostRequest.getName()
+		);
+	}
 }
