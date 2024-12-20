8366e76f603dceab7b6be6a0b654f81bb77a9f6f
u-genuine <u_genuine@naver.com>
2024-12-20 19:12:16 +0900

    [Refactor] @Notnull 어노테이션 추가

--- backend/src/main/java/cotato/backend/domains/post/dto/request/SavePostRequest.java
+++ backend/src/main/java/cotato/backend/domains/post/dto/request/SavePostRequest.java
@@ -1,10 +1,17 @@
 package cotato.backend.domains.post.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class SavePostRequest {
+
+	@NotNull
 	String title;
+
+	@NotNull
 	String content;
+
+	@NotNull
 	String name;
 }
